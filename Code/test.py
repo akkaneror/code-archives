7e598208aef5d3308e6be808c3777252055d1699
akkaneror <akkaneror@gmail.com>
2019-03-27 01:00:03 -0700

    Add some quickSort to test, because quickSort is quick and why not?

--- Code/test.py
+++ Code/test.py
@@ -1,28 +1,53 @@
-# qsort with partition
-def quickSortPartition(alist):
-    quickSortHelper(alist, 0, len(alist) - 1)
+# Python program for implementation of Quicksort Sort
+
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+def partition(arr, low, high):
+    i = (low - 1)  # index of smaller element
+    pivot = arr[high]  # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j] <= pivot:
+            # increment index of smaller element
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
 
 
-def quickSortHelper(alist, first, last):
-    if first < last:
-        splitpoint = partition(alist, first, last)
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low --> Starting index,
+# high --> Ending index
 
-        quickSortHelper(alist, first, splitpoint - 1)
-        quickSortHelper(alist, splitpoint + 1, last)
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if low < high:
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+    # Driver code to test above
 
 
-def partition(alist, first, last):
-    pivotvalue = alist[first]
-
-    leftmark = first + 1
-    rightmark = last
-
-    done = False
-    while not done:
-
-        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-            leftmark = leftmark + 1
-
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
 
 # good ol' qsort
 def quickSortBasic(x):
@@ -64,7 +89,7 @@ def quickSort3Way(array):
         return array
 
 
-quickSortBasic([6, 4, 7, 1, 2, 9, 12, 3])
+quickSortPartition([6, 4, 7, 1, 2, 9, 12, 3])
 
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 quickSortPartition(alist)
